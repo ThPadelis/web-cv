911f75db8acc21e869a85a97f4a7f8395589246a
unknown <padelis.theodosiou@gmail.com>
2020-06-21 19:20:58 +0300

    Added an alternative navbar for small screens

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,17 +1,25 @@
 <template>
-  <b-navbar toggleable="lg" type="light" variant="white">
-    <div></div>
-    <b-navbar-toggle target="nav-collapse" class="border-0"></b-navbar-toggle>
+  <div>
+    <b-navbar class="d-none d-lg-block" toggleable="lg" type="light" variant="white">
+      <div></div>
+      <b-navbar-toggle target="nav-collapse" class="border-0"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav fill justified class="w-100">
-        <b-nav-item v-for="(n, i) in navItems" :key="i" :to="n.to" active-class="active-item">
-          <span :class="n.icon" class="mr-1"></span>
-          <span>{{ n.text }}</span>
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav fill justified class="w-100">
+          <b-nav-item v-for="(n, i) in navItems" :key="i" :to="n.to" active-class="active-item">
+            <span :class="n.icon" class="mr-1"></span>
+            <span>{{ n.text }}</span>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <b-nav fill justified class="alt-nav d-lg-none">
+      <b-nav-item v-for="(n,i) in navItems" :key="i" :to="n.to">
+        <span :class="n.icon"></span>
+      </b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
@@ -30,7 +38,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navbar {
+.navbar,
+.alt-nav {
   -webkit-box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
   padding: 0;
@@ -45,6 +54,7 @@ export default {
       text-transform: uppercase;
       padding: 1.5rem 1.5rem calc(1.5rem - 3px) 1.5rem;
       transition: all ease-in-out 0.25s;
+      color: $dark;
 
       &:hover,
       &.active-item {
